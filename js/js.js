2b56e09fdbb3daf599d440e85fd5a3c98ec281fd
Cleiton <tirandoonda2014@gmail.com>
2022-01-31 19:16:11 -0300

    Finalizado movimentação, inimigos e amigo

--- js/js.js
+++ js/js.js
@@ -39,6 +39,8 @@ function start() { // Inicio da função start()
 		movefundo();
 		movejogador();
 		moveinimigo1();
+		moveinimigo2();
+		moveamigo();
 	} // Fim da função loop()
 
 
@@ -75,7 +77,7 @@ function start() { // Inicio da função start()
 		}
 	
 	} // fim da função movejogador()
-	
+
 	function moveinimigo1() {
 
 		posicaoX = parseInt($("#inimigo1").css("left"));
@@ -89,6 +91,30 @@ function start() { // Inicio da função start()
 				
 			}
 	} //Fim da função moveinimigo1()
+
+	function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+	$("#inimigo2").css("left",posicaoX-3);
+				
+		if (posicaoX<=0) {
+			
+		$("#inimigo2").css("left",775);
+					
+		}
+} // Fim da função moveinimigo2()
+
+function moveamigo() {
+	
+	posicaoX = parseInt($("#amigo").css("left"));
+	$("#amigo").css("left",posicaoX+1);
+				
+		if (posicaoX>906) {
+			
+		$("#amigo").css("left",0);
+					
+		}
+
+} // fim da função moveamigo()
 
 } // Fim da função start
 
